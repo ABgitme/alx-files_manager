240bd0828d23e8d3c50fa1e9db788a3b502ebfba
ABgitme <abebekezhabesha@gmail.com>
2024-12-07 08:26:06 +0300

    Get and list file

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -79,6 +79,70 @@ class FilesController {
       ...fileDocument,
     });
   }
+
+  // Retrieve file by ID
+  static async getShow(req, res) {
+    const token = req.headers['x-token'];
+
+    // Authenticate user using token
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const { id } = req.params;
+
+    try {
+      const db = dbClient.client.db(dbClient.databaseName);
+      const file = await db.collection('files').findOne({
+        _id: new ObjectId(id),
+        userId: new ObjectId(userId),
+      });
+
+      if (!file) {
+        return res.status(404).json({ error: 'Not found' });
+      }
+
+      return res.status(200).json(file);
+    } catch (error) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+  }
+
+  // Retrieve files with pagination
+  static async getIndex(req, res) {
+    const token = req.headers['x-token'];
+
+    // Authenticate user using token
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const { parentId = 0, page = 0 } = req.query;
+
+    const limit = 20; // Max items per page
+    const skip = parseInt(page, 10) * limit;
+
+    try {
+      const db = dbClient.client.db(dbClient.databaseName);
+
+      // Query files based on parentId
+      const query = { userId: new ObjectId(userId) };
+      if (parentId !== 0) {
+        query.parentId = parentId === '0' ? '0' : new ObjectId(parentId);
+      }
+
+      const files = await db
+        .collection('files')
+        .aggregate([{ $match: query }, { $skip: skip }, { $limit: limit }])
+        .toArray();
+
+      return res.status(200).json(files);
+    } catch (error) {
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+  }
 }
 
 export default FilesController;
