e1801065cf7edf6fb23a5144e541dd28f79e9505
ABgitme <abebekezhabesha@gmail.com>
2024-12-09 07:46:23 +0300

    File data

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
-/*
 import AuthController from '../controllers/AuthController';
+/*
 import FilesController from '../controllers/FilesController';
 */
 
@@ -10,15 +10,14 @@ const router = express.Router();
 // GET routes
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 // POST routes
 router.post('/users', UsersController.postNew);
 /*
 router.post('/files', FilesController.postUpload);
 
-router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', UsersController.getMe);
 router.get('/files/:id', FilesController.getShow);
 router.get('/files', FilesController.getIndex);
 router.get('/files/:id/data', FilesController.getFile);
