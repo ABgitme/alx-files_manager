3b10480bd89875d60ae9f041cd33a1f935946d97
ABgitme <abebekezhabesha@gmail.com>
2024-12-11 14:04:13 +0300

    routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
 router.post('/users', UsersController.postNew);
 router.post('/files', FilesController.postUpload);
-/*
+
 router.get('/files/:id', FilesController.getShow);
 router.get('/files', FilesController.getIndex);
 router.get('/files/:id/data', FilesController.getFile);
@@ -24,5 +24,5 @@ router.get('/files/:id/data', FilesController.getFile);
 // PUT routes
 router.put('/files/:id/publish', FilesController.putPublish);
 router.put('/files/:id/unpublish', FilesController.putUnpublish);
-*/
+
 export default router;
