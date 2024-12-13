6f3d9be66d4612332bda2423f9b2535cab4eb9b7
ABgitme <abebekezhabesha@gmail.com>
2024-12-13 07:08:27 +0300

    FilesController index fix

--- routes/index.js
+++ routes/index.js
@@ -10,21 +10,16 @@ router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
-
 router.get('/users/me', UsersController.getMe);
-router.post('/users', UsersController.postNew);
-// PUT routes
-router.put('/files/:id/publish', FilesController.putPublish);
-router.put('/files/:id/unpublish', FilesController.putUnpublish);
-
-/*
-router.post('/files', FilesController.postUpload);
-
 router.get('/files/:id', FilesController.getShow);
 router.get('/files', FilesController.getIndex);
 router.get('/files/:id/data', FilesController.getFile);
 
 // POST routes
+router.post('/users', UsersController.postNew);
+router.post('/files', FilesController.postUpload);
+// PUT routes
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
 
-*/
 export default router;
